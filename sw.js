7a24a2ab70d30667d3a01e21dc8546f428292739
robertaroos-lgtm <robert_a_roos@hotmail.com>
2025-10-18 18:29:16 +0200

    Add files via upload
    add PWA-files

---
+++ sw.js
@@ -0,0 +1,30 @@
+// Enkel cache för offline-stöd
+const CACHE = "laxkollen-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sw.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
