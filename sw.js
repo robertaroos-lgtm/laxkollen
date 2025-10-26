913809260678931b84c696150ab6445cb4fdec45
robertaroos-lgtm <robert_a_roos@hotmail.com>
2025-10-26 08:39:25 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='lk-v13.4.9';
+const C='lk-v13.4.10';
 self.addEventListener('install',function(e){e.waitUntil(caches.open(C).then(function(c){return c.addAll(['./','./index.html','./style.css?v=13.4.3','./app.js?v=13.4.3','./manifest.json']);}));self.skipWaiting();});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.filter(function(k){return k!==C;}).map(function(k){return caches.delete(k);}));}));self.clients.claim();});
 self.addEventListener('fetch',function(e){if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request);}));});
